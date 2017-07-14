c0ccbf8fdaf1059f1c9bd0e2cf9f7247a86dec22
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-14 11:03:45 -0600

    adds styling to form inputs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
+
   before_save :generate_slug
+
   mount_uploader :image, ImageUploader
+  
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
   validates :slug, uniqueness: true
+  
   has_one :picture, as: :imageable
+  
   accepts_nested_attributes_for :picture
 
   enum role: %w(admin)
